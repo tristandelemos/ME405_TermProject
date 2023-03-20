11d5b1a74f2fdb0f8080bc1f5a5c51afa70dcb20
tristandelemos <71288767+tristandelemos@users.noreply.github.com>
2023-03-19 18:55:45 -0700

    tryin to work images

--- docs/search/all_7.js
+++ docs/search/all_7.js
@@ -2,11 +2,12 @@ var searchData=
 [
   ['main_0',['main',['../main_8py.html#af613cea4cba4fb7de8e40896b3368945',1,'main.main()'],['../motor__reader_8py.html#a0802ee4db6e8b202bf4c94c1e4782a28',1,'motor_reader.main()']]],
   ['main_2epy_1',['main.py',['../main_8py.html',1,'']]],
-  ['mlx_5fcam_2',['MLX_Cam',['../classmlx__cam_1_1_m_l_x___cam.html',1,'mlx_cam.MLX_Cam'],['../classmlx__cam__mod_1_1_m_l_x___cam.html',1,'mlx_cam_mod.MLX_Cam']]],
-  ['mlx_5fcam_2epy_3',['mlx_cam.py',['../mlx__cam_8py.html',1,'']]],
-  ['mlx_5fcam_5fmod_2epy_4',['mlx_cam_mod.py',['../mlx__cam__mod_8py.html',1,'']]],
-  ['motor_5fdriver_2epy_5',['motor_driver.py',['../motor__driver_8py.html',1,'']]],
-  ['motor_5freader_2epy_6',['motor_reader.py',['../motor__reader_8py.html',1,'']]],
-  ['motordriver_7',['MotorDriver',['../classmotor__driver_1_1_motor_driver.html',1,'motor_driver']]],
-  ['my_20personal_20index_20page_8',['My Personal Index Page',['../index.html',1,'']]]
+  ['mainpage_2',['mainpage',['../md_src_mainpage.html',1,'']]],
+  ['mlx_5fcam_3',['MLX_Cam',['../classmlx__cam_1_1_m_l_x___cam.html',1,'mlx_cam.MLX_Cam'],['../classmlx__cam__mod_1_1_m_l_x___cam.html',1,'mlx_cam_mod.MLX_Cam']]],
+  ['mlx_5fcam_2epy_4',['mlx_cam.py',['../mlx__cam_8py.html',1,'']]],
+  ['mlx_5fcam_5fmod_2epy_5',['mlx_cam_mod.py',['../mlx__cam__mod_8py.html',1,'']]],
+  ['motor_5fdriver_2epy_6',['motor_driver.py',['../motor__driver_8py.html',1,'']]],
+  ['motor_5freader_2epy_7',['motor_reader.py',['../motor__reader_8py.html',1,'']]],
+  ['motordriver_8',['MotorDriver',['../classmotor__driver_1_1_motor_driver.html',1,'motor_driver']]],
+  ['my_20personal_20index_20page_9',['My Personal Index Page',['../index.html',1,'']]]
 ];
--- docs/search/pages_0.js
+++ docs/search/pages_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['my_20personal_20index_20page_0',['My Personal Index Page',['../index.html',1,'']]]
+  ['mainpage_0',['mainpage',['../md_src_mainpage.html',1,'']]],
+  ['my_20personal_20index_20page_1',['My Personal Index Page',['../index.html',1,'']]]
 ];
